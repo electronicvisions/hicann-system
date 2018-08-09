9e805055b77845d874c9c5520bda1d049b0cb3b7
Andreas Grübl <agruebl@kip.uni-heidelberg.de>
2018-08-09 16:51:00 +0200

    make tm_hicann_sram_reset use tests2's comm model and HICANN selection
    Change-Id: I85220679d1a9eb18ac6798b190c159a16e19d455

--- units/hicann_test/source/tm_hicann_sram_reset.cpp
+++ units/hicann_test/source/tm_hicann_sram_reset.cpp
@@ -19,7 +19,7 @@ protected:
 	virtual string ClassName() { stringstream ss; ss << "tm_hicann_sram_reset"; return ss.str(); }
 
 public:
-	HicannCtrl *hc, *hc_jtag;
+	HicannCtrl *hc;
 
 	//sets the repeater frequency (use e.g. 2,1 to get 100 MHz)
 	//frequency = multiplicator * 50 MHz / divisor
@@ -39,85 +39,67 @@ public:
 	{
 
 		bool result  = true;
-		uint hicannr = 0;
 
 		// SRAM controller timing (used for all controllers)
 		uint read_delay      = 64;
 		uint setup_precharge = 8;
 		uint write_delay     = 8;
 
-		if (!jtag) {
-		 	log(Logger::ERROR) << "object 'jtag' in testmode not set, abort";
-			return 0;
-		}
-
-		// assume JTAG object is valid, generate JTAG-only comm stuff locally:
-		S2C_JtagPhys* s2c_jtag;
-		s2c_jtag   = new S2C_JtagPhys(comm->getConnId(), jtag, false);
-		hc_jtag    = new HicannCtrl(s2c_jtag,hicannr);
-
 		// set to 125 MHz
 		set_pll(5,2);
 
-		// assume that only comm init stuff and no resets are neccessary...
+		// use HICANN number 0 (following FPGA and DNC in address space) for this testmode
+		uint hicannr = 0;
+		hc = (HicannCtrl*) chip[FPGA_COUNT+DNC_COUNT+hicannr];
+		
+		// ----------------------------------------------------
+		// Initialize comm class (using the one generated in tests2)
+		// ----------------------------------------------------
+	 	log(Logger::INFO) << "Try Init() ..." ;
+		if (hc->GetCommObj()->Init(hc->addr(), false, false) != Stage2Comm::ok) {
+		 	log(Logger::ERROR) << "ERROR: Init failed, abort" << endl;
+			return 0;
+		}
+	 	log(Logger::INFO) << "Init() ok" << endl;
 
-		// ####################################################################################
-		// loop over HICANNs in reticle:
-		for(uint hc_jtnr = 0; hc_jtnr < 8; hc_jtnr++){
+		log(Logger::INFO) << "Setting all full-custom SRAMS of HICANN " << hc->addr() << " to 0";
 
-			jtag->set_hicann_pos(hc_jtnr);
-			hc_jtag->setAddr(hc_jtnr);
+		hc->getSPL1Control().write_reset();
 
-			// ----------------------------------------------------
-			// Initialize JTAG only comm for initial readback: ARQ reset and other stuff
-			// ----------------------------------------------------
-			if (hc_jtag->GetCommObj()->Init(hc_jtag->addr()) != Stage2Comm::ok) {
-				log(Logger::ERROR) << "Init failed, abort";
-				return 0;
-			}
-
-			log(Logger::INFO) << "Setting all full-custom SRAMS of HICANN " << hc_jtnr << " to 0";
-
-			hc_jtag->getSPL1Control().write_reset();
-
-			// assume that all enum values are consecutive (true for HW indices)
-			// and counted C-style (top-left to bottom-right)
-			for(uint i = HicannCtrl::L1SWITCH_TOP_LEFT ; i != HicannCtrl::L1SWITCH_BOTTOM_RIGHT+1 ; i++) {
-				hc_jtag->getLC(i).reset();
-			}
-
-			for(uint i = HicannCtrl::REPEATER_TOP_LEFT ; i != HicannCtrl::REPEATER_BOTTOM_RIGHT+1 ; i++) {
-				// set SRAM controller timings, first:
-				hc_jtag->getRC(i).set_sram_timings(read_delay, setup_precharge, write_delay);
-				hc_jtag->getRC(i).reset();
-			}
-
-			for(uint i = HicannCtrl::SYNAPSE_TOP ; i != HicannCtrl::SYNAPSE_BOTTOM+1 ; i++) {
-				// set SRAM controller timings, first:
-				hc_jtag->getSC(i).set_sram_timings(read_delay, setup_precharge, write_delay);
-				hc_jtag->getSC(i).reset_drivers();
-			}
-
-			// FG controller reset has no real effect (no custom srams there) - doesn't have to be used.
-			//for(uint i=HicannCtrl::FG_TOP_LEFT ; i != HicannCtrl::FG_BOTTOM_RIGHT ; i++)
-			//	hc_jtag->getFC(i).reset();
-
-			// set SRAM controller timings, first:
-			hc_jtag->getNBC().set_sram_timings(read_delay, setup_precharge, write_delay);
-			hc_jtag->getNBC().reset();
-
-			hc_jtag->getNC().nc_reset();
-
-			// super nice parametrization...
-			if (std::getenv("I_KNOW_WHAT_I_AM_CLEARING") != NULL) {
-				log(Logger::INFO) << "Setting all synapses of HICANN " << hc_jtnr << " to 0";
-				hc_jtag->getSC(HicannCtrl::SYNAPSE_TOP).reset_all();
-				hc_jtag->getSC(HicannCtrl::SYNAPSE_BOTTOM).reset_all();
-			}
+		// assume that all enum values are consecutive (true for HW indices)
+		// and counted C-style (top-left to bottom-right)
+		for(uint i = HicannCtrl::L1SWITCH_TOP_LEFT ; i != HicannCtrl::L1SWITCH_BOTTOM_RIGHT+1 ; i++) {
+			hc->getLC(i).reset();
 		}
 
-		delete s2c_jtag;
-		delete hc_jtag;
+		for(uint i = HicannCtrl::REPEATER_TOP_LEFT ; i != HicannCtrl::REPEATER_BOTTOM_RIGHT+1 ; i++) {
+			// set SRAM controller timings, first:
+			hc->getRC(i).set_sram_timings(read_delay, setup_precharge, write_delay);
+			hc->getRC(i).reset();
+		}
+
+		for(uint i = HicannCtrl::SYNAPSE_TOP ; i != HicannCtrl::SYNAPSE_BOTTOM+1 ; i++) {
+			// set SRAM controller timings, first:
+			hc->getSC(i).set_sram_timings(read_delay, setup_precharge, write_delay);
+			hc->getSC(i).reset_drivers();
+		}
+
+		// FG controller reset has no real effect (no custom srams there) - doesn't have to be used.
+		//for(uint i=HicannCtrl::FG_TOP_LEFT ; i != HicannCtrl::FG_BOTTOM_RIGHT ; i++)
+		//	hc->getFC(i).reset();
+
+		// set SRAM controller timings, first:
+		hc->getNBC().set_sram_timings(read_delay, setup_precharge, write_delay);
+		hc->getNBC().reset();
+
+		hc->getNC().nc_reset();
+
+		// super nice parametrization...
+		if (std::getenv("I_KNOW_WHAT_I_AM_CLEARING") != NULL) {
+			log(Logger::INFO) << "Setting all synapses of HICANN " << hc->addr() << " to 0";
+			hc->getSC(HicannCtrl::SYNAPSE_TOP).reset_all();
+			hc->getSC(HicannCtrl::SYNAPSE_BOTTOM).reset_all();
+		}
 
 		return result;
 	}
